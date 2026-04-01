48c5a0fa62a00ef787a41273e1e5b5c9b281cbc5
Zainab Ayaz <qayaz9465@gmail.com>
2026-04-02 03:13:41 +0500

    Update appointment time slots to start from 4:30 PM

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -23,9 +23,8 @@ const Appointment = () => {
   ];
 
   const timeSlots = [
-    '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM',
-    '11:00 AM', '11:30 AM', '02:00 PM', '02:30 PM',
-    '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM',
+    '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM',
+    '06:30 PM', '07:00 PM',
   ];
 
   const handleChange = (e) => {
